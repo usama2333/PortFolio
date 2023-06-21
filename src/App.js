7ca43ea29516c68ffce0543a0325e832a2457e71
usama2333 <usama.wizz@gmail.com>
2023-06-21 13:35:26 +0500

    Added home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-// import Preloader from "../src/components/Pre";
+import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 // import About from "./components/About/About";
 // import Projects from "./components/Projects/Projects";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 // import Resume from "./components/Resume/ResumeNew";
 import {
   BrowserRouter as Router,
@@ -12,7 +12,7 @@ import {
   Routes,
   Navigate
 } from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
+// import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -33,7 +33,7 @@ function App() {
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
-        <ScrollToTop />
+        {/* <ScrollToTop /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           {/* <Route path="/project" element={<Projects />} />
